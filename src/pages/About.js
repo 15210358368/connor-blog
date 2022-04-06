2506d63887362f8392f52dcfd85312b8cff0dec9
Peng Wang <38886423+15210358368@users.noreply.github.com>
2022-04-05 20:03:46 -0700

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const About = () =>{
+    return(
+        <div className="mb-20">
+            <h1
+            className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">
+                About
+                </h1>
+            <p className="mx-auto leading-relaxed text-base mb-4">This is a dynamic list and may never be able to satisfy particular standards for completeness. You can help by adding missing items with reliable sources.
+This is a list of common misconceptions. Each entry is worded as a correction; the misconceptions themselves are implied rather than stated. These entries are concise summaries of the main subject articles, which can be consulted for more detail.
+
+A common misconception is a viewpoint or factoid that is often accepted as true in current times. They often arise from conventional wisdom (such as old wives' tales), stereotypes, a misunderstanding of science, or popularisation of pseudoscience. Some common misconceptions are also considered to be urban legends, and they are often involved in moral panics.
+            </p>
+            <p className="mx-auto leading-relaxed text-base mb-4">This is a dynamic list and may never be able to satisfy particular standards for completeness. You can help by adding missing items with reliable sources.
+This is a list of common misconceptions. Each entry is worded as a correction; the misconceptions themselves are implied rather than stated. These entries are concise summaries of the main subject articles, which can be consulted for more detail.
+
+A common misconception is a viewpoint or factoid that is often accepted as true in current times. They often arise from conventional wisdom (such as old wives' tales), stereotypes, a misunderstanding of science, or popularisation of pseudoscience. Some common misconceptions are also considered to be urban legends, and they are often involved in moral panics.
+            </p>
+            <p className="mx-auto leading-relaxed text-base mb-4">This is a dynamic list and may never be able to satisfy particular standards for completeness. You can help by adding missing items with reliable sources.
+This is a list of common misconceptions. Each entry is worded as a correction; the misconceptions themselves are implied rather than stated. These entries are concise summaries of the main subject articles, which can be consulted for more detail.
+
+A common misconception is a viewpoint or factoid that is often accepted as true in current times. They often arise from conventional wisdom (such as old wives' tales), stereotypes, a misunderstanding of science, or popularisation of pseudoscience. Some common misconceptions are also considered to be urban legends, and they are often involved in moral panics.
+            </p>
+        </div>
+    )
+}
+
+export default About 
---
+++ src/pages/Article.js
@@ -0,0 +1,35 @@
+import React from "react";
+import articleContent from "./ArticleContent";
+import {useParams} from "react-router-dom";
+import Articles from '../component/Articles'
+import NotFound from "./NotFound";
+
+const Article = () =>{
+    const { name } = useParams();
+    
+    const article = articleContent.find((article) => {
+        if(article.name === name) return article;
+    });
+    if(!article) return <NotFound />
+    const otherArticles = articleContent.filter((article) => article.name !== name);
+    return(
+        <div className="mb-20">
+            <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">
+                { article && article.title}
+            </h1>
+            {article && article.content.map((paragraph, index) =>{
+                return  <p className="mx-auto leading-relaxed text-base mb-4" key = {index}>{paragraph}</p>
+            }
+               
+            )}
+            <h1 className='sm:text-2x text-xl font-bold mt-4 mb-4 text-gray-900'>
+                    Other Articles
+                </h1>
+            <div className='flex flex-wrap -m-4'>
+                    <Articles article={otherArticles} />
+            </div>
+        </div>
+    )
+}
+
+export default Article;
